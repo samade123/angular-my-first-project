425e34e6395429cb24eabff50e21f8119793465b
Samuel Ademola <samuel.ademola43@gmail.com>
2023-10-25 13:41:16 +0100

    advanced attribute directives

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'yellow'
+  @Input() appHighlight = '';
+
+  @HostListener("mouseenter") onMouseEnter() {
+    this.highlight(this.appHighlight)
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.highlight('')
+  }
+
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.backgroundColor = this.appHighlight
+   }
+
+   private highlight(color:string){
+    this.el.nativeElement.style.backgroundColor = color;
+
    }
 
 }
